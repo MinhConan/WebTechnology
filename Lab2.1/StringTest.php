4b9733c1cbf5d3b9b3f1a0c3a6aacac380ba4f9f
toai-php <56766134+toai-php@users.noreply.github.com>
2021-03-31 20:09:15 +0700

    Create StringTest.php

---
+++ Lab2.1/StringTest.php
@@ -0,0 +1,16 @@
+
+<HTML>
+    <HEAD> <TITLE>String Test </TITLE> </HEAD>
+    <BODY>
+        <?PHP
+            $str1 = "this is ";
+            $str2 = "a string";
+            $str3 = $str1 . $str2;
+            print "string 1 is: $str1";
+            print "<br>string 2 is: $str2";
+            print "<br>string 3 is str1+str2: $str3";
+            
+        ?>
+    </BODY>
+</HTML>
+
